daa5ac2f71a5bb6ba6c68c2cbc503f6f110aee2e
shailendra Adhav <shailendra.adhav@gmail.com>
2020-09-12 20:33:33 +0530

    tuplesupdated

---
+++ tuples.ts
@@ -0,0 +1,5 @@
+const drink = { color: 'brown', corbonated: true, sugar: 40 };
+
+type drink = [string, boolean, number];
+const thumsup: drink = ['brown', true, 40];
+const sprite: drink = ['brown', true, 2];
