93a262e0b3526f61ec075474c4585f7c0d639745
shailendra Adhav <shailendra.adhav@gmail.com>
2020-09-16 02:11:51 +0530

    added google maps up marker

---
+++ maps/src/CustomMaps.ts
@@ -0,0 +1,30 @@
+//Instruction to every other class on how to they can be an argument to 'addMaker'
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById('map'), {
+      zoom: 2,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+  }
+}
